b5dc25437fb073414de524dc99ef88ef4522b796
sam <sf663@cam.ac.uk>
2021-02-12 12:30:08 +0000

    Changed UserView to use org.joml.Vector2f/Vector2i

---
+++ src/uk/ac/cam/cl/groupprojectdelta/galtonboards/workspace/UserView.java
@@ -0,0 +1,48 @@
+package uk.ac.cam.cl.groupprojectdelta.galtonboards.workspace;
+
+import org.joml.Vector2f;
+import org.joml.Vector2i;
+
+public class UserView {
+
+    private Vector2f position;
+    private float scale;
+    private Vector2i displaySize;
+    private Vector2f displayHalfSize;
+
+    public UserView(Vector2f position, float scale, Vector2i displaySize) {
+        this.position = position;
+        this.scale = scale;
+        this.displaySize = displaySize;
+        this.displayHalfSize = new Vector2f(displaySize.x / 2.0f, displaySize.y / 2.0f);
+    }
+
+    public Vector2f getPosition() {
+        return position;
+    }
+
+    public Vector2i getDisplaySize() {
+        return displaySize;
+    }
+
+    public void setPosition(Vector2f position) {
+        this.position = position;
+    }
+
+    public void setDisplaySize(Vector2i displaySize) {
+        this.displaySize = displaySize;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public float getWorldPos(Vector2i pixelPos) {
+        throw new UnsupportedOperationException();  // Matt said he'd implement this
+    }
+
+}
