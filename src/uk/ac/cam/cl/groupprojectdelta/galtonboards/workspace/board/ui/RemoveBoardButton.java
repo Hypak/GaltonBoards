a948d9615fee49786335271deea12c136fc6f4eb
ali <amw223@cam.ac.uk>
2021-03-01 18:19:49 +0000

    Implemented Drawable methods for WorkspaceButtons.

--- src/uk/ac/cam/cl/groupprojectdelta/galtonboards/workspace/board/ui/RemoveBoardButton.java
+++ src/uk/ac/cam/cl/groupprojectdelta/galtonboards/workspace/board/ui/RemoveBoardButton.java
@@ -3,7 +3,8 @@ package uk.ac.cam.cl.groupprojectdelta.galtonboards.workspace.board.ui;
 import org.joml.Vector2f;
 
 public class RemoveBoardButton extends WorkspaceButton {
-  private Vector2f getPosition() {
+  @Override
+  protected Vector2f getPosition() {
     return null;
   }
 
